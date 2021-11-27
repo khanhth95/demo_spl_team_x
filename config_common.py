04729638ec455086d2d9f494441a9c0a81e150b7
khanhth95 <73108809+khanhth95@users.noreply.github.com>
2021-11-27 12:09:48 +0700

    Create config_common.py

---
+++ config_common.py
@@ -0,0 +1,23 @@
+user_line = 10
+
+cp_dec_limit=150
+cp_limit = 1500
+cp_enough = 1000
+rent_days = 1
+ecr_limit=50
+headless = True
+min_cp = 500
+max_cp = 1000
+
+#How many accounts run in the same time
+parrallel_acc_num = 1
+
+#Rent and run config
+RENT_CARD = True
+RENT_CP = True
+RUN_BOT = True
+
+card_rent_list = {
+#'Card name':[card_id, 'Level x', max_dec, market_url]
+'Venari Wavesmith':[338, 'Level 1', 8,"https://cache-api.splinterlands.com/market/for_rent_by_card?card_detail_id=338&gold=false&edition=3"],
+}
